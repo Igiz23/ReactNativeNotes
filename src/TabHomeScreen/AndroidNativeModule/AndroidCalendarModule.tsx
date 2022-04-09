d3cb43f2a76e04561e2e9eae9701e0f67bdefc0b
Aygiz Adigamov <aadigamov@smartdatatech.io>
2022-04-10 02:51:01 +0300

    start calendar module

---
+++ src/TabHomeScreen/AndroidNativeModule/AndroidCalendarModule.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const AndroidCalendarModule = ({navigation}: any) => {
+  navigation.void;
+  return (
+    <View>
+      <Text>{'Android Calendar Module'}</Text>
+    </View>
+  );
+};
